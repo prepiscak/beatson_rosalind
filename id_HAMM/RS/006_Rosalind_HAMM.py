a0676dd1fc32c6b627e8654d9ad2eb80b8f17913
rwsBeatson <r.shaw@beatson.gla.ac.uk>
2019-04-08 20:48:01 +0100

    Add data checks and multiple ways to add data - rws

--- id_HAMM/RS/006_Rosalind_HAMM.py
+++ id_HAMM/RS/006_Rosalind_HAMM.py
@@ -11,25 +11,61 @@ Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
 Return: The Hamming distance dH(s,t).
 '''
 import os
+import re
+
+
+def allowed_match(strg, search=re.compile(r'[^GCTA.]').search):
+    return not bool(search(strg))
 
 
 def hamm(s, t):
     dH = 0
-    for l in range(0, len(s) - 1):
+    for l in range(0, len(s)):
         if s[l] != t[l]:
             dH += 1
     return dH
 
+
 def test():
-    test_s = "GAGCCTACTAACGGGAT"
-    test_t = "CATCGTAATGACGGCCT"
+    test_s = "GAGCCTACTAACGGGATA"
+    test_t = "CATCGTAATGACGGCCTG"
+    print(test_s)
+    print(test_t)
     print("Hamming Distance:")
     print(hamm(test_s, test_t))
 
+
+def inputSequence():
+    s_i = input("Input first sequence ")
+    t_i = input("Input second sequence: ")
+    if s_i == 'q' or t_i == 'q':
+        print("Quiting")
+        exit()
+    elif len(s_i) != len(t_i):
+        print("Sequences of different length. Please re-enter")
+        inputSequence()
+    elif allowed_match(s_i) or allowed_match(t_i):
+        print("Illegal characters in sequence. Please re-enter")
+        inputSequence()
+    else:
+       print(hamm(s_i, t_i))  
+
+
+def check_file(lst):
+    if len(lst[0]) != len(lst[1]):
+        return False
+    elif allowed_match(lst[0]) or allowed_match(lst[1]):
+        return False
+    else:
+        return True
+
+
 def main():
-    fn = input("Please enter path, 'test' for test data or 'q' to quit: ")
+    fn = input("Please enter path, 'test' for test data, 'input' to input sequences or 'q' to quit\n")
     if fn == 'test':
         test()
+    elif fn == 'input':
+        inputSequence()
     elif fn == 'q':
         exit()
     else:
@@ -37,13 +73,17 @@ def main():
         exists = os.path.isfile(fn)
         if exists:
             # Store configuration file values
-            print("Found")
+            print("File found")
             # Load data and split into two
             with open(fn, 'r') as fhand:
                 dt = fhand.read().split('\n')
-                print("Hamming Distance:")
-                print(hamm(dt[0], dt[1]))
-
+                    #check_file(dt)
+                if True:
+                    print("Hamming Distance:")
+                    print(hamm(dt[0], dt[1]))
+                else:
+                    print("Sorry, problem with sequences in file")
+                    main()
         else:
             print("Sorry, couldn't find the file.")
             print()
